80778b199c7c2352b1fcc1104023ead812f46d08
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-10-20 19:24:18 +0530

    ok

---
+++ number_even_or_odd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(num) {
+    if (num % 2 === 0) {
+        console.log(num, "is even");
+    }
+    if (num % 2 !== 0) {
+      console.log(num, "is odd");
+    }
+}
+
+evenOrOdd(1015);
