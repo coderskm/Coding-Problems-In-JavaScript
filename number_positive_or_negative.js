54ab4d7153c3a25d0c269a3ee5b256ca74de91fa
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-10-20 19:21:26 +0530

    ok

---
+++ number_positive_or_negative.js
@@ -0,0 +1,10 @@
+function positiveOrNegative(num) {
+    if (num > 0) {
+        console.log(num, "is positve");
+    }
+    if (num < 0) {
+        console.log(num, "is negative");
+    }
+}
+
+positiveOrNegative(-7.90000000);
